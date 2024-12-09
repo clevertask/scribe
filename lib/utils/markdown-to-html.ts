22cf37b445628e8a66ce54fab0762b93a086b48a
Roberto Piñon <rpinon@hearsaycorp.com>
2024-12-09 00:12:01 -0600

    chore: use isomorphic dompurify and export tip tap type

--- lib/utils/markdown-to-html.ts
+++ lib/utils/markdown-to-html.ts
@@ -1,5 +1,4 @@
 import { marked } from "marked";
-import DOMPurify from "dompurify";
+import DOMPurify from "isomorphic-dompurify";
 
-export const md2html = (md: string) =>
-  DOMPurify.sanitize(marked.parse(md, { async: false }));
+export const md2html = (md: string) => DOMPurify.sanitize(marked.parse(md, { async: false }));
